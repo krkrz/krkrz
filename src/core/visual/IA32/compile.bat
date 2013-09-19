9dbb39dc400ecc5305755c13cea9496d289012d3
jin1016 <info@kaede-software.com>
2013-09-19 17:08:30 +0900

    tvpgl_ia32 のビルドをプロジェクトに追加

--- src/core/visual/IA32/compile.bat
+++ src/core/visual/IA32/compile.bat
@@ -1,8 +1,8 @@
 echo off
 del tvpgl_ia32.lib
 del *.obj
-for %%d in (*.nas) do nasmw -g -f obj %%d
-for %%d in (*.obj) do tlib tvpgl_ia32.lib + %%d
+REM for %%d in (*.nas) do nasmw -g -f obj %%d
+REM for %%d in (*.obj) do tlib tvpgl_ia32.lib + %%d
 perl summary.pl
 del *.bak
 perl copy_if_differ.pl tvpgl_ia32_intf_.c tvpgl_ia32_intf.c
