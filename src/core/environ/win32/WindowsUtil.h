be80db4be69b0208b01663d78a936e3c8c571bcf
jin1016 <info@kaede-software.com>
2013-11-02 02:12:40 +0900

    Windows XP で動作するように修正。

--- src/core/environ/win32/WindowsUtil.h
+++ src/core/environ/win32/WindowsUtil.h
@@ -4,5 +4,8 @@
 
 extern void TVPThrowWindowsErrorException();
 extern void TVPOutputWindowsErrorToDebugMessage();
+extern void TVPOutputWindowsErrorToConsole( const char* file=NULL, int line=0 );
+
+#define TVP_WINDOWS_ERROR_LOG TVPOutputWindowsErrorToConsole( __FILE__, __LINE__ );
 
 #endif	// __WINDOWS_ERROR_EXCEPTION_H__
