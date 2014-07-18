e414c8e0d294c03cc343abdcca681b5ee136fac2
Kyoh Mikami <mikami@dsl.gr.jp>
2014-07-18 20:03:51 +0900

    マウスイベントのタッチ操作判定を変更／プレスアンドホールドの右クリック操作を無効化
    WM_MOUSE*イベントのタッチ判定は ::GetMessageExtraInfo() の値で判定できるため，そちらを使って判定するように変更しました。
    それに伴い ignore_touch_mouse_ メンバ変数を削除しました。
    
    WM_MOUSEMOVEでも判定有効なので，タッチ操作でOnMouseMoveを呼ばないようにしました。
    （実マウスカーソル座標は移動しますが，吉里吉里側のイベントは飛びません）
    
    プレスアンドホールドジェスチャで右クリックイベントが飛んできたので，これも無効化する設定にしました。
    他，フィードバック系も無効にしてあります。tTVPWindow::DEFAULT_TABLETPENSERVICE_PROPERTY の設定を確認してください。
    参考： http://msdn.microsoft.com/ja-jp/library/windows/desktop/dd562171(v=vs.85).aspx

--- src/core/environ/win32/TVPWindow.h
+++ src/core/environ/win32/TVPWindow.h
@@ -53,7 +53,6 @@ protected:
 	SIZE		max_size_;
 	int			border_style_;
 	bool		in_window_;
-	bool		ignore_touch_mouse_;
 
 	bool in_mode_;
 	int modal_result_;
@@ -64,6 +63,7 @@ protected:
 	static const UINT POS_CHANGE_FLAGS;
 	static const DWORD DEFAULT_EX_STYLE;
 	static const ULONG REGISTER_TOUCH_FLAG;
+	static const DWORD DEFAULT_TABLETPENSERVICE_PROPERTY;
 
 	bool left_double_click_;
 
@@ -135,7 +135,7 @@ protected:
 public:
 	tTVPWindow()
 	: window_handle_(NULL), created_(false), left_double_click_(false), ime_control_(NULL), border_style_(0), modal_result_(0),
-		in_window_(false), ignore_touch_mouse_(false), in_mode_(false), has_parent_(false) {
+		in_window_(false), in_mode_(false), has_parent_(false) {
 		min_size_.cx = min_size_.cy = 0;
 		max_size_.cx = max_size_.cy = 0;
 	}
