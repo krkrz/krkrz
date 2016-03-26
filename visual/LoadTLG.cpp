b3dc23cb531d3bff37c873f62c8adcf9eea424be
Takenori Imoto <info@kaede-software.com>
2016-03-27 04:03:47 +0900

    画像読み書きプラグイン化対応。画像ヘッダー読み込み対応。WIC画像プラグイン対応。

--- visual/LoadTLG.cpp
+++ visual/LoadTLG.cpp
@@ -14,6 +14,7 @@
 #include "MsgIntf.h"
 #include "tjsUtils.h"
 #include "tvpgl.h"
+#include "tjsDictionary.h"
 
 #include <stdlib.h>
 
@@ -566,4 +567,67 @@ void TVPLoadTLG(void* formatdata, void *callbackdata, tTVPGraphicSizeCallback si
 
 }
 //---------------------------------------------------------------------------
+void TVPLoadHeaderTLG(void* formatdata, tTJSBinaryStream *src, iTJSDispatch2** dic )
+{
+	if( dic == NULL ) return;
 
+	// read header
+	unsigned char mark[12];
+	src->ReadBuffer(mark, 11);
+
+	tjs_int width = 0;
+	tjs_int height = 0;
+	tjs_int colors = 0;
+	// check for TLG0.0 sds
+	if(!memcmp("TLG0.0\x00sds\x1a\x00", mark, 11))
+	{
+		// read raw data size
+		tjs_uint rawlen = src->ReadI32LE();
+		src->ReadBuffer(mark, 11);
+		if(!memcmp("TLG5.0\x00raw\x1a\x00", mark, 11))
+		{
+			src->ReadBuffer(mark, 1);
+			colors = mark[0];
+			width = src->ReadI32LE();
+			height = src->ReadI32LE();
+		}
+		else if(!memcmp("TLG6.0\x00raw\x1a\x00", mark, 11))
+		{
+			src->ReadBuffer(mark, 4);
+			colors = mark[0]; // color component count
+			width = src->ReadI32LE();
+			height = src->ReadI32LE();
+		}
+		else
+		{
+			TVPThrowExceptionMessage(TVPTLGLoadError, (const tjs_char*)TVPInvalidTlgHeaderOrVersion );
+		}
+	}
+	else if(!memcmp("TLG5.0\x00raw\x1a\x00", mark, 11))
+	{
+		src->ReadBuffer(mark, 1);
+		colors = mark[0];
+		width = src->ReadI32LE();
+		height = src->ReadI32LE();
+	}
+	else if(!memcmp("TLG6.0\x00raw\x1a\x00", mark, 11))
+	{
+		src->ReadBuffer(mark, 4);
+		colors = mark[0]; // color component count
+		width = src->ReadI32LE();
+		height = src->ReadI32LE();
+	}
+	else
+	{
+		TVPThrowExceptionMessage(TVPTLGLoadError, (const tjs_char*)TVPInvalidTlgHeaderOrVersion );
+	}
+	tjs_int bpp = colors * 8;
+	*dic = TJSCreateDictionaryObject();
+	tTJSVariant val(width);
+	(*dic)->PropSet(TJS_MEMBERENSURE, TJS_W("width"), 0, &val, (*dic) );
+	val = tTJSVariant(height);
+	(*dic)->PropSet(TJS_MEMBERENSURE, TJS_W("height"), 0, &val, (*dic) );
+	val = tTJSVariant(bpp);
+	(*dic)->PropSet(TJS_MEMBERENSURE, TJS_W("bpp"), 0, &val, (*dic) );
+}
+//---------------------------------------------------------------------------
