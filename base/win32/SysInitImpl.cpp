797fd6e3e491852a977746452e85b212bc45940e
Takenori Imoto <info@kaede-software.com>
2016-08-18 17:31:39 +0900

    VC2015で構造体のメンバ名が変更になっていたので修正。

--- base/win32/SysInitImpl.cpp
+++ base/win32/SysInitImpl.cpp
@@ -621,7 +621,7 @@ void TVPDumpOSContext(const CONTEXT &ctx)
 	}
 
 	// -- Cr0NpxState
-	TJS_snprintf(buf, BUF_SIZE,TJS_W("FP CR0 NPX State  : 0x%08X"), ctx.FloatSave.Cr0NpxState);
+	TJS_snprintf(buf, BUF_SIZE,TJS_W("FP CR0 NPX State  : 0x%08X"), ctx.FloatSave.Spare0);
 	TVPAddLog(buf);
 #endif
 
