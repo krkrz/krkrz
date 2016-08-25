927847f9791ce23d7b0dacc3b0360594e14073d6
Takenori Imoto <info@kaede-software.com>
2016-08-26 03:29:04 +0900

    makefileに本体のソースコード追加

--- Application.mk
+++ Application.mk
@@ -4,6 +4,7 @@ APP_BUILD_SCRIPT := $(APP_PROJECT_PATH)/Android.mk
 #APP_ABI :=all
 APP_ABI := armeabi-v7a
 APP_PLATFORM := android-20
-
-
+APP_STL := stlport_static
+#APP_STL := gnustl_static
+APP_MODULES := libfreetype libjpeg-turbo libonig libpng krkrz
 
