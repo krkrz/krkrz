7bccffd296d7137a6fc47a72f0a8196bc50d1178
jin1016 <info@kaede-software.com>
2016-10-19 02:34:02 +0900

    文字列リソース生成

---
+++ msg/text/gentext_android.bat
@@ -0,0 +1,9 @@
+perl gentext_android.pl
+copy tjsErrorInc.h ..\..\tjs2\tjsErrorInc.h
+copy MsgImpl.h ..\android\MsgImpl.h
+copy MsgIntfInc.h ..\MsgIntfInc.h
+copy MsgLoad.cpp ..\android\MsgLoad.cpp
+REM copy string_table_en.cpp ..\android\string_table_en.cpp
+copy string_table_jp.cpp ..\android\string_table_jp.cpp
+REM copy string_table_resource.h ..\android\string_table_resource.h
+pause
