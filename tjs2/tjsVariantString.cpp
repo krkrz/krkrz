c751275eed4fcdad4bb0282566f604cf347a132b
jin1016 <info@kaede-software.com>
2017-01-09 03:23:48 +0900

    I64 to ll for String.sprintf

--- tjs2/tjsVariantString.cpp
+++ tjs2/tjsVariantString.cpp
@@ -904,11 +904,10 @@ tTJSVariantString * TJSFormatString(const tjs_char *format, tjs_uint numparams,
 			tjs_uint fmtlen = (tjs_uint)(f - fst);
 			if(fmtlen > 65) goto error;  // too long
 			TJS_strncpy(fmt, fst, fmtlen);
-			fmt[fmtlen] = TJS_W('I'); //// CHECK!! 'I64' must indicate a 64bit integer
-			fmt[fmtlen+1] = TJS_W('6');
-			fmt[fmtlen+2] = TJS_W('4');
-			fmt[fmtlen+3] = *f;
-			fmt[fmtlen+4] = 0;
+			fmt[fmtlen] = TJS_W('l'); //// CHECK!! 'll' must indicate a 64bit integer
+			fmt[fmtlen+1] = TJS_W('l');
+			fmt[fmtlen+2] = *f;
+			fmt[fmtlen+3] = 0;
 			int ind[2];
 			if(!width_ind && !prec_ind)
 			{
