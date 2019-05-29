9d01f0fe5fc8cb82959039e9a4e981e68d843269
uyjulian <uyjulian@gmail.com>
2019-05-29 14:59:39 -0500

    Add aligned_allocator C++17 and MinGW support

--- visual/gl/aligned_allocator.h
+++ visual/gl/aligned_allocator.h
@@ -3,12 +3,13 @@
 #ifndef __ALIGNED_ALLOCATOR_H__
 #define __ALIGNED_ALLOCATOR_H__
 
-#ifdef _WIN32
+#if defined(_MSC_VER)
 #include <intrin.h>
+#elif defined(__MINGW32__)
+#include <malloc.h>		// _aligned_malloc and _aligned_free
 #else
 #include <stdlib.h>
 #endif
-#include <malloc.h>		// _aligned_malloc and _aligned_free
 #include <memory>		// std::allocator
 
 
@@ -23,21 +24,33 @@ struct aligned_allocator : public std::allocator<T>
 	template <class U> aligned_allocator(const aligned_allocator<U, TAlign>&) throw() {}
 	template <class U> aligned_allocator& operator=(const aligned_allocator<U, TAlign>&) throw()  {}
 	// allocate
-#ifdef _WIN32
+#if defined(_MSC_VER)
 	pointer allocate(size_type c, const void* hint = 0) {
 		return static_cast<pointer>( _mm_malloc( sizeof(T)*c, TAlign ) );
 	}
-#else
+#elif defined(__MINGW32__)
+	T* allocate(std::size_t c) {
+		return static_cast<T*>( _aligned_malloc( sizeof(T)*c, TAlign ) );
+	}
+#elif defined(__unix__) || (defined(__APPLE__) && defined(__MACH__))
 	T* allocate( std::size_t c ) {
 		T* ret;
 		posix_memalign(reinterpret_cast<void**>(&ret), TAlign, sizeof(T)*c);
 		return ret;
 	}
+#else
+	T* allocate( std::size_t c ) {
+		return static_cast<T*>( aligned_alloc( TAlign, sizeof(T)*c ) );
+	}
 #endif
 	// deallocate
-#ifdef _WIN32
+#if defined(_MSC_VER)
 	void deallocate(pointer p, size_type n) {
 		_mm_free( p );
+	}
+#elif defined(__MINGW32__)
+	void deallocate( T* p, std::size_t n ) {
+		_aligned_free( p );
 	}
 #else
 	void deallocate( T* p, std::size_t n ) {
