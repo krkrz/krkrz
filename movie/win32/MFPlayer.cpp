014ae4bcf01eef2c02fe4db58939d0c1c6cdf72c
uyjulian <uyjulian@gmail.com>
2019-07-14 08:43:52 -0500

    Explicitly convert HRESULT to tjs_int

--- movie/win32/MFPlayer.cpp
+++ movie/win32/MFPlayer.cpp
@@ -611,7 +611,7 @@ void __stdcall tTVPMFPlayer::ReleaseAll()
 }
 //----------------------------------------------------------------------------
 void tTVPMFPlayer::NotifyError( HRESULT hr ) {
-	TVPThrowExceptionMessage(TJS_W("MF Operation Error."),hr);
+	TVPThrowExceptionMessage(TJS_W("MF Operation Error."),(tjs_int)hr);
 }
 void tTVPMFPlayer::OnMediaItemCleared() {
 }
