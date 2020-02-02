a31607e0fccc1dc5eece6fa8aee85c0c3b317362
uyjulian <uyjulian@gmail.com>
2020-02-02 10:33:37 -0600

    Avoid _set_se_translator call on compiler configurations where it is not supported

--- environ/win32/Application.cpp
+++ environ/win32/Application.cpp
@@ -329,7 +329,9 @@ const tjs_char* SECodeToMessage( unsigned int code ) {
 }
 
 bool tTVPApplication::StartApplication( int argc, tjs_char* argv[] ) {
+#if (!defined(__GNUC__)) || (defined(__GNUC__) && defined (__SEH__) && !defined (__USING_SJLJ_EXCEPTIONS__))
 	_set_se_translator(se_translator_function);
+#endif
 
 	ArgC = argc;
 	ArgV = argv;
